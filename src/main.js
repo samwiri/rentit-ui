c618c33bf88970c7e2a683349e69c76d77e731b3
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2020-03-25 01:54:30 +0300

    add tailwind for utility classes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+import "./styles/tailwind.scss";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount("#app");
